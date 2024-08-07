37b1e65270ecf51bea170715e7a6416decab857f
MemoDADR <memodealbadelriego@hotmail.com>
2024-08-06 20:33:01 -0600

    test

---
+++ Rapberry-Js/example.js
@@ -0,0 +1,31 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("/home/guillermo/MICO/PowerTIc/PowerTIC/Rapberry-Js/power-tic-firebase-adminsdk-9u1tt-190471deb9.json");
+const childProcess = require('child_process');
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  // The database URL depends on the location of the database
+  databaseURL: "https://power-tic-default-rtdb.firebaseio.com/"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("/Meters/E3T15060693/Firmware");
+ref.on('child_changed', (snapshot) => {
+    const changedPost = snapshot.val();
+    console.log('The updated post title is ' + changedPost);
+    sh('bash /home/guillermo/MICO/PowerTIc/PowerTIC/try.sh')
+  });
+async function sh(cmd_to_execute) {
+    return new Promise(function (resolve, reject) {
+        childProcess.exec(cmd_to_execute, (err, stdout, stderr) => {
+            if (err) {
+              reject(err);
+              } else {
+                resolve({stdout, stderr});
+              }
+          });
+      });
+  }
+  
--- try.sh
+++ try.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 shell=True
-sleep 10s
+exec 3>&1 1>"log.log" 2>&1
+cd ~
+cd MICO/PowerTIc/PowerTIC
+git remote set-url origin git@github.com:user/repo.git
+git pull https://github.com/AVargas-C/PowerTIC
+echo 'MAMO'
+git checkout JSOTA
 git pull
 echo "update success"
-python deploy.py
-
-fi
+source .venv/bin/activate
+python Rapberry/deploy.py

96b9dbc3cf63accbcb6ab8103bbc8deccb80777d
Luis Antonio Sánchez Islas <luis08islas@gmail.com>
2024-08-21 13:30:19 -0600

    mejorado

--- Rapberry/Infoupload.py
+++ Rapberry/Infoupload.py
@@ -1,6 +1,23 @@
 import psycopg2
 
 # PostgreSQL Init
+
+connlocal= psycopg2.connect(
+    user="postgres",
+    host="localhost",
+    database="powerTic",
+    password="Tono2002",  # luis: Tono2002 //Arturo: 2705
+    port=5432
+)
+
+with connlocal.cursor() as cursor:
+    cursor.execute("SELECT * FROM powertic.locations;")
+    rows = cursor.fetchall()
+    
+    # Fetch column names
+    column_names = [desc[0] for desc in cursor.description]
+    
+
 conn = psycopg2.connect(
     user="superadmin",
     host="powerticpgtest1.postgres.database.azure.com",
@@ -10,16 +27,13 @@ conn = psycopg2.connect(
 )
 
 with conn.cursor() as cursor:
-    cursor.execute("SELECT * FROM powertic.locations;")
-    rows = cursor.fetchall()
-    
-    # Fetch column names
-    column_names = [desc[0] for desc in cursor.description]
-    
+    print(rows)
+    print(column_names)
     # Prepare the SQL INSERT statement with placeholders for parameters
     columns_str = ", ".join(column_names)
     placeholders = ", ".join(["%s"] * len(column_names))
-    
+    print(columns_str)
+    print(placeholders)
     insert_query = f"INSERT INTO powertic.locationsbackup ({columns_str},mockid) VALUES ({placeholders},1234)"
 
     # Insert each row into the locationsbackup table

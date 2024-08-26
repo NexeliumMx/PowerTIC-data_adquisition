b1e81d6f0c11fe8b17b424f78a5f9021d40dabd4
Arturo Vargas Cuevas <avargas2705@outlook.com>
2024-08-26 17:54:55 -0600

    add Headermenu

--- WebApp/src/pages/Dashboard/Dashboard.jsx
+++ WebApp/src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar.jsx';
+import Navbar from '../../components/Navbar/Navbar.jsx';
+import Header from '../../components/Header/Header.jsx';
+import HeaderMenu from './components/HeaderMenu'; // Import the specific HeaderMenu for Dashboard
 import './Dashboard.scss';  // Import the Dashboard-specific styles
 
 function Dashboard() {
@@ -7,6 +10,8 @@ function Dashboard() {
     <div className="dashboard-container">
       <Sidebar />
       <div className="dashboard-content">
+        <Navbar title="Dashboard" />
+        <Header headerMenu={HeaderMenu} /> {/* Pass HeaderMenu as a prop */}
         <h1>Dashboard</h1>
         {/* Other content goes here */}
       </div>

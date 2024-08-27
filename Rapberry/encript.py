f8fc859449f09aaa26e91b84b7ebbd2351d9b1e7
MemoDADR <memodealbadelriego@hotmail.com>
2024-08-26 23:59:40 -0600

    jkjhjk

--- Rapberry/encript.py
+++ Rapberry/encript.py
@@ -1,6 +1,6 @@
 def encript(value):
     f=open(r"Rapberry/sn.txt","r")
-    key=str(hash(f.read()))[1:len(f.read)]
+    key=str(hash(f.read()))[1:len(str(f.read()))]
     print(key)
     out=''
     for i in range(0,len(value)):

70a82682e07050b349798d14e4cd069599ce31cd
Arturo Vargas Cuevas <avargas2705@outlook.com>
2024-08-28 11:07:22 -0600

    remove temporary sidebar menu from downloads page

--- WebApp/src/pages/Downloads/Downloads.jsx
+++ WebApp/src/pages/Downloads/Downloads.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar.jsx';
 import Navbar from '../../components/Navbar/Navbar.jsx';
+import Header from './components/Header.jsx';
 import './Downloads.scss';
 
 function Downloads() {
   return (
     <div className="page-container">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="page-content">
         <Navbar title="Downloads" />
         {/* The rest of the page will be blank */}
+        <Header />
       </div>
     </div>
   );
---
+++ WebApp/src/pages/Downloads/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import './Header.scss';
+
+function Header() {
+
+  return (
+    <div className="header-container">
+      <div className="header-titles">
+        <h1>Centro de Carga 1</h1>
+        <h2>Centro de Carga 1</h2>
+        <h3>San Ángel</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

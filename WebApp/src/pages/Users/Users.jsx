e1d7c63149a1e1b44d44526915e3c29d6b701b88
rogerpww2002 <rogealg@gmail.com>
2024-08-28 19:20:41 -0600

    Ajustes página users

--- WebApp/src/pages/Users/Users.jsx
+++ WebApp/src/pages/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar.jsx';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import Header from './Components/Header.jsx';
+import Tile from './Components/Userstile.jsx';
 import './Users.scss';
 
 function Users() {
@@ -9,9 +10,27 @@ function Users() {
     <div className="page-container">
       <Sidebar/>
       <div className="page-content">
-        <Navbar title="Users" />
-        <Header/>
-        {/* The rest of the page will be blank */}
+        <Navbar title="Users"/>
+
+        <div className='user-1'>
+          <Tile title='SAN ÁNGEL' number='1' /> 
+        </div>
+        <div className='user-2'>
+          <Tile title='CLUB AMÉRICA' number='2' /> 
+        </div>
+        <div className='user-3'>
+          <Tile title='ESTADIO AZTECA' number='1' /> 
+        </div>
+        <div className='user-4'>
+          <Tile title='RADIOPOLIS' number='1' /> 
+        </div>
+        <div className='user-5'>
+          <Tile title='CORP. SANTA FE' number='4' /> 
+        </div>
+        <div className='user-6'>
+          <Tile title='CENTRO DE CARGA 6' number='#' /> 
+        </div>
+        
       </div>
     </div>
   );

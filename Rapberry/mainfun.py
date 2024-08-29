788afebf09fff7d65d9ed30b2a121101cbf4703b
MemoDADR <memodealbadelriego@hotmail.com>
2024-08-28 20:09:55 -0600

    forgot smth in mainfunct

--- Rapberry/mainfun.py
+++ Rapberry/mainfun.py
@@ -6,7 +6,7 @@ import subprocess
 import time
 # From Python3.7 you can add 
 # keyword argument capture_output
-print(subprocess.run(["mkdir", "Geeks for geeks"], 
+print(subprocess.run(["mkdir", "vals"], 
                      capture_output=True))
 a=0
 while(1):

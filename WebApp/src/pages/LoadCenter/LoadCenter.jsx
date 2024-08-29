ea0d1680f1b580465f1f186a75bce3797483d4f8
rogerpww2002 <rogealg@gmail.com>
2024-08-28 20:21:37 -0600

    Cambio de tabla de centros de carga de la pestaña usuarios a la pestaña de centros de carga

--- WebApp/src/pages/LoadCenter/LoadCenter.jsx
+++ WebApp/src/pages/LoadCenter/LoadCenter.jsx
@@ -1,15 +1,26 @@
+import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar.jsx';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 
-export default function LoadCenter() {
+import LCTile from './Components/LCTile.jsx'; // Asegúrate de que la ruta sea correcta
+import './LoadCenter.scss';
+
+export default function Users() {
   return (
     <div className="page-container">
       <Sidebar />
-
       <div className="page-content">
-        <Navbar title="Centro de Carga" />
+        <Navbar title="Centros de Carga" />
+        <div className="LC-tile-container"> {/* Este es el contenedor grid */}
+          <LCTile title='SAN ÁNGEL' number='1' /> 
+          <LCTile title='CLUB AMÉRICA' number='2' /> 
+          <LCTile title='ESTADIO AZTECA' number='1' /> 
+          <LCTile title='RADIOPOLIS' number='1' /> 
+          <LCTile title='CORP. SANTA FE' number='4' /> 
+          <LCTile title='CENTRO DE CARGA 6' number='#' /> 
+        </div>
       </div>
-      
     </div>
   );
 }
+
--- WebApp/src/pages/Users/Users.jsx
+++ WebApp/src/pages/Users/Users.jsx
@@ -1,36 +1,26 @@
+import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar.jsx';
 import Navbar from '../../components/Navbar/Navbar.jsx';
-import Header from './Components/Header.jsx';
-import Tile from './Components/Userstile.jsx';
+
+import UsersTile from './Components/Userstile.jsx'; // Asegúrate de que la ruta sea correcta
 import './Users.scss';
 
 export default function Users() {
   return (
     <div className="page-container">
-      <Sidebar/>
+      <Sidebar />
       <div className="page-content">
-        <Navbar title="Users"/>
-
-        <div className='user-1'>
-          <Tile title='SAN ÁNGEL' number='1' /> 
+        <Navbar title="Users" />
+        <div className="users-tile-container"> {/* Este es el contenedor grid */}
+          <UsersTile title='SAN ÁNGEL' number='1' /> 
+          <UsersTile title='CLUB AMÉRICA' number='2' /> 
+          <UsersTile title='ESTADIO AZTECA' number='1' /> 
+          <UsersTile title='RADIOPOLIS' number='1' /> 
+          <UsersTile title='CORP. SANTA FE' number='4' /> 
+          <UsersTile title='CENTRO DE CARGA 6' number='#' /> 
         </div>
-        <div className='user-2'>
-          <Tile title='CLUB AMÉRICA' number='2' /> 
-        </div>
-        <div className='user-3'>
-          <Tile title='ESTADIO AZTECA' number='1' /> 
-        </div>
-        <div className='user-4'>
-          <Tile title='RADIOPOLIS' number='1' /> 
-        </div>
-        <div className='user-5'>
-          <Tile title='CORP. SANTA FE' number='4' /> 
-        </div>
-        <div className='user-6'>
-          <Tile title='CENTRO DE CARGA 6' number='#' /> 
-        </div>
-        
       </div>
     </div>
   );
 }
+

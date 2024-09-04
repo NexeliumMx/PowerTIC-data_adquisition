2636e3fcd8a4e1d9c8342155e0ee250c2de3c525
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-09-04 13:27:36 -0600

    changes

--- data_acquisition/Rapberry/clonefromcloud.py
+++ data_acquisition/Rapberry/clonefromcloud.py
@@ -12,7 +12,7 @@ azure_conn = psycopg2.connect(
 
 # Fetch data from Azure PostgreSQL
 with azure_conn.cursor() as cursor:
-    cursor.execute("SELECT * FROM powertic.locations;")
+    cursor.execute("SELECT * FROM powertic.modbusqueries;")
     rows = cursor.fetchall()
     
     # Fetch column names
@@ -23,7 +23,7 @@ local_conn = psycopg2.connect(
     user="postgres",
     host="localhost",
     database="postgres",
-    password="Tono2002",  # Replace with your actual password
+    password="postgres",  # Replace with your actual password
     port=5432
 )
 
@@ -32,7 +32,7 @@ with local_conn.cursor() as cursor:
     # Prepare the SQL INSERT statement with placeholders for parameters
     columns_str = ", ".join(column_names)
     placeholders = ", ".join(["%s"] * len(column_names))
-    insert_query = f"INSERT INTO public.localtest ({columns_str}, mockid) VALUES ({placeholders}, 1234)"
+    insert_query = f"INSERT INTO public.modbusqueries {columns_str} VALUES {placeholders}"
     
     # Insert each row into the localtest table
     for row in rows:
@@ -51,7 +51,7 @@ with local_conn.cursor() as cursor:
     local_conn.commit()
 
     # Optional: Verify that the data has been inserted
-    cursor.execute("SELECT * FROM public.localtest;")
+    cursor.execute("SELECT * FROM public.modbusqueries;")
     inserted_rows = cursor.fetchall()
     print("Inserted rows into local PostgreSQL:")
     for inserted_row in inserted_rows:

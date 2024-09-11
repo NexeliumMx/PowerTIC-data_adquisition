ceaf1dbe85f71955aa155e570db57fe6413ec11e
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-09-10 18:29:04 -0600

    add

--- Rapberry/addnewqueries.py
+++ Rapberry/addnewqueries.py
@@ -10,23 +10,38 @@ local_conn = psycopg2.connect(
     port=5432
 )
 
-
+# Read the Excel file
 df = pd.read_excel('NewModbusQueries.xlsx')
 
-column_names= df.columns.tolist()
+# Get column names
+column_names = df.columns.tolist()
 print(column_names)
 
-for index, row in df.iterrows():
-    print(row.tolist())
+# Construct the insert query
+placeholders = ", ".join(["%s"] * len(column_names))
+insert_query = f'INSERT INTO public.modbusqueries ({", ".join(column_names)}) VALUES ({placeholders})'
 
+# Loop through rows and process data
 with local_conn.cursor() as cursor:
-    placeholders = ", ".join(["%s"] * len(column_names))
-    insert_query = f'INSERT INTO powertic.modbusqueries({column_names}) VALUES ('
     for index, row in df.iterrows():
-        row = list(row)
-        modbus_address = column_names.index('modbus_address')
-        if isinstance(row[modbus_address],list):
-            row[modbus_address] = 'ARRAY [' + ','.join(map(str,row(modbus_address)))+']'
-            print(row[modbus_address])
-        elif isinstance(row[modbus_address], int):
-            row[modbus_address] = '{'+ str(row[modbus_address]) + '}'
+        row_data = list(row)
+
+        # Handle modbus_address if it's a list or int
+        modbus_address_index = column_names.index('modbus_address')
+        
+        if isinstance(row_data[modbus_address_index], list):
+            row_data[modbus_address_index] = '{' + ','.join(map(str, row_data[modbus_address_index])) + '}'
+        elif isinstance(row_data[modbus_address_index], int):
+            row_data[modbus_address_index] = '{' + str(row_data[modbus_address_index]) + '}'
+
+        # Convert row data to tuple for cursor execution
+        row_data_tuple = tuple(row_data)
+        
+        # Execute the insert query
+        cursor.execute(insert_query, row_data_tuple)
+
+    # Commit the transaction
+    local_conn.commit()
+
+# Close the connection
+local_conn.close()

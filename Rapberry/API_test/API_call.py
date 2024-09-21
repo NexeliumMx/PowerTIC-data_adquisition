bda260088c76acbee448e340b30c4e019d341891
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-09-21 12:59:59 -0600

    local api

--- Rapberry/API_test/API_call.py
+++ Rapberry/API_test/API_call.py
@@ -2,22 +2,24 @@ import requests
 import json
 
 # Define the data with your specific fields
-data = {
-    "client": 'ABC',         # Replace with actual client data
-    "broker": 'DEF',         # Replace with actual broker data
-    "cloud_services": False,     # Replace with actual cloud service data
-    "payment": True,             # Replace with actual payment data
-    "payment_amount": 1000      # Replace with actual payment amount
-}
+data =[
+    {
+        "table": 'clients'
+    }, {
+        "client": 'AEI',         # Replace with actual client data
+        "broker": 'DEF',         # Replace with actual broker data
+        "cloud_services": False,     # Replace with actual cloud service data
+        "payment": True,             # Replace with actual payment data
+        "payment_amount": 1000      # Replace with actual payment amount
+    }
+]
 
 # URL of the API endpoint
-url = "https://powertic.azurewebsites.net/api/sql_api"
+url = "http://localhost:7071/api/sql_manager"
 
-# Send the POST request with the JSON-encoded data
-response = requests.post(url, json=data)
-
-# Check the response
-if response.status_code == 200:
-    print('Success:')  # If the response is in JSON format
-else:
-    print('Error:', response.status_code, response.text)
+try:
+    response = requests.post(url, json=data)
+    response.raise_for_status()  # Raises HTTPError if the status is 4xx, 5xx
+    print('Success:', response.text)
+except requests.exceptions.RequestException as e:
+    print('HTTP Request failed:', e)

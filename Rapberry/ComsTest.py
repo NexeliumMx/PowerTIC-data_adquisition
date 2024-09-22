87960f98dedacfbbe7367a4c345c90ec6eb380ff
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-09-22 12:18:27 -0600

    try

--- Rapberry/ComsTest.py
+++ Rapberry/ComsTest.py
@@ -2,7 +2,8 @@ from pymodbus.client import ModbusSerialClient
 import psycopg2
 import json
 import requests
-
+import datetime
+from datetime import timezone
 #conxión con el servidor local de postgres
 conn = psycopg2.connect(
     user="postgres",
@@ -85,4 +86,88 @@ def meter_param():
     print(data)
     print(settings.get("serial_number", "serial_number not found"))
     return settings.get('serial_number')
-meter_param()
+
+def reading_meter(sn):
+    # Constantes
+    measurement = {}
+    table_name = {}
+    table_name["table"] = "measurements"
+    # Extracting Modbus addresses from the CSV
+    with conn.cursor() as cursor:
+        cursor.execute(f"SELECT parameter_description, modbus_address, register_number,indb FROM powertic.modbusqueries")
+        rows = cursor.fetchall()
+
+        if client.connect():
+            try:        
+                for row in rows:
+                    #Debug
+                    #print(row)
+                    #print(row[0])
+                    #print(row[1])
+                    #print(row[1][0])  
+                    if row[3]==True:
+                        if isinstance(row[1][0], list):
+                            #print(row[1][0][0]) 
+                            parameter = row[0]
+                            
+                            modbus_address = row[1][0][0]
+                            # Iterate over each address in the list
+                            for modbus_address in row[1][0]:
+                                try:
+                                    # Data acquisition for each address
+                                    meas = client.read_holding_registers(modbus_address, 1)
+                                    if not meas.isError():
+                                        meas_val = meas.registers[0]
+                                        measurement[f"{parameter}"] = meas_val
+                                    else:
+                                        print(f"Error de lectura {parameter} en {modbus_address}:", meas)
+                                except ValueError:
+                                    print(f"Invalid address for {parameter}: {modbus_address}")
+
+                        else:
+                            parameter = row[0]
+                            modbus_address = row[1][0]
+                            # Data acquisition for a single address
+                            meas = client.read_holding_registers(modbus_address, 1)
+                            if not meas.isError():
+                                meas_val = meas.registers[0]
+                                measurement[f"{parameter}"] = meas_val
+                                print(f"{parameter}: {meas_val}")
+                                
+                            else:
+                                print(f"Error de lectura {parameter} en {modbus_address}", meas)
+            except Exception as e:
+                print("Exception:", e)
+
+            finally:
+
+                client.close()
+
+                timestamp = datetime.datetime.now(timezone.utc).replace(tzinfo=None).isoformat() + "Z"
+                measurement["timestamp"] = timestamp
+                json_data = [table_name, measurement]
+                data = json.dumps(json_data)
+                
+                """url = "https://powertic-apis-js.azurewebsites.net/api/sql_manager"
+                response = requests.post(url, json=data)
+
+                if response.status_code == 200:
+                    print('Success:')
+                else:
+                    print('Error:', response.status_code, response.text)"""
+                
+                #debug
+                print(table_name)
+                print(measurement)
+                print(data)
+            
+                return data
+            
+        else:
+            print("Error de conexión con el medidor")
+
+#debug
+#print(reading_meter())
+
+reading_meter()
+#meter_param()

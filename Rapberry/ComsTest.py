3f1dad0412e9e187af8266f2880bb57b7b9a7fe1
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-09-25 17:14:02 -0600

    try

--- Rapberry/ComsTest.py
+++ Rapberry/ComsTest.py
@@ -57,7 +57,8 @@ def meter_param():
                 for row in rows: 
                     #print(row)
                     #print(row["parameter_description"],row["modbus_address"])      
-                    if row['setup'] == True: 
+                    print(row["setup"],type(row["setup"]), bool(row["setup"]),type(bool(row["setup"])) )      
+                    if row['setup'] == "t": 
                         #print("parameter: ", row["parameter_description"] )
                         parameter = row['parameter_description']
                         #print("Parameter: ",parameter)

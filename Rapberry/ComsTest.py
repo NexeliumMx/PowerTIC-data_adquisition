dbe905e10be418b5f52b5b7942fa5ef1437c9897
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-09-25 16:19:18 -0600

    meter_param

--- Rapberry/ComsTest.py
+++ Rapberry/ComsTest.py
@@ -222,6 +222,6 @@ def reading_meter(sn):
         
 #debug
 #print(reading_meter())
-#meter_param()
+meter_param()
 #reading_meter(meter_param())
-print(reading_meter("E3T15060693"))
+#print(reading_meter("E3T15060693"))

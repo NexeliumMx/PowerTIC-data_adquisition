a86590ceb7610bdf71aa6cb525d8054d26109db9
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-09-30 19:03:35 -0600

    fix

--- Rapberry/ComsTest.py
+++ Rapberry/ComsTest.py
@@ -184,7 +184,7 @@ def reading_meter(sn):
                                     if not meas.isError():
                                         for i in meas.registers:
                                             meas_val += chr((i & 0b1111111100000000)>>8) + chr(i & 0b0000000011111111)
-                                            meas_val = meas_val.replace('\x00','')
+                                        meas_val = meas_val.replace('\x00','')
                                         print("measurement value: ",meas_val)
                                         measurement[f'{parameter_description}'] = meas_val
                                     else:

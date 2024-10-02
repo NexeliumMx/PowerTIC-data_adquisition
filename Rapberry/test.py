b9a5ea3f222137798acffe5057b88beff55d2d21
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-10-02 14:03:39 -0600

    try 4

--- Rapberry/test.py
+++ Rapberry/test.py
@@ -39,7 +39,7 @@ try:
         print(f"Successfully wrote {value_to_write} to register {register_address}")
 
     register_address = 0x20A  # 522 in decimal
-    value_to_write = 0x02  # Equivalent to 0b0000000000000010
+    value_to_write = 0x04  # Equivalent to 0b0000000000000010
     response = client.write_registers(register_address, [value_to_write], 1)
 
     # Check if the write was successful

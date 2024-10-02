322c9c506fabb20fc33c36b1ec4d28f3b6cab457
MemoDADR <memodealbadelriego@hotmail.com>
2024-10-02 11:42:31 -0600

    try

--- Rapberry/resetregisters.py
+++ Rapberry/resetregisters.py
@@ -38,7 +38,7 @@ else:
     print(f"Successfully wrote {value_to_write} to registers {register_address} and {register_address+1}")
 register_address = 0x20B  # The first register address (adjust for 0-based if necessary)
 value_to_write = 0b0000000000000000  
-response = client.write_registers(register_address, value_to_write, 1)
+response = client.write_registers(register_address, [value_to_write,0], 1)
 
 # Check if the write was successful
 if response.isError():

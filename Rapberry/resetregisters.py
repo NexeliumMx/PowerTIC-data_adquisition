d0fcc3f128e4b21c7ddd1c18f297cb688b638b5f
MemoDADR <memodealbadelriego@hotmail.com>
2024-10-02 11:46:50 -0600

    try

--- Rapberry/resetregisters.py
+++ Rapberry/resetregisters.py
@@ -30,7 +30,7 @@ else:
     print(f"Successfully wrote {value_to_write} to registers {register_address} and {register_address+1}")
 # Close the Modbus connection
 register_address = 0x20A  # The first register address (adjust for 0-based if necessary)
-value_to_write = 0x02 
+value_to_write = 0x000000100000000 
 response = client.write_registers(register_address, value_to_write, 1)
 if response.isError():
     print(f"Failed to write to register {register_address}: {response}")
@@ -46,7 +46,7 @@ if response.isError():
 else:
     print(f"Successfully wrote {value_to_write} to registers {register_address} and {register_address+1}")
 register_address = 0x20D  # The first register address (adjust for 0-based if necessary)
-value_to_write = 0b1111111011001100  
+value_to_write = 0b1100110000000000  
 response = client.write_registers(register_address, value_to_write, 1)
 if response.isError():
     print(f"Failed to write to register {register_address}: {response}")

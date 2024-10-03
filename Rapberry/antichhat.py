c729cbc3d88ad4584d3df05e6afb7efa0c3f3028
MemoDADR <memodealbadelriego@hotmail.com>
2024-10-02 19:14:17 -0600

    try

--- Rapberry/antichhat.py
+++ Rapberry/antichhat.py
@@ -5,5 +5,5 @@ serialPort = serial.Serial(
 a=modbus.TinyModbusClient(serial_connection=serialPort,
                                        crc_enabled=True,
                                        timeout=1.0)
-d=b'0x02/0x0B/0x00/0x01/0x01/0x00/0x02'
+d=b'0x02/0x0B/0x00/0x01/0x01/0x00/0x00'
 a.send_custom_message(1,3,d)

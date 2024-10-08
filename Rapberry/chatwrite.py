c8feab450b396406e868b608f4bd273c4474a9db
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-10-08 17:01:23 -0600

    try

--- Rapberry/chatwrite.py
+++ Rapberry/chatwrite.py
@@ -122,7 +122,7 @@ def write_modbus_registers(slave_address, function_code, starting_address, quant
 
 #write_modbus(slave_address=slave_address,function_code=function_code,starting_address=starting_address, quantity_of_registers=quantity_of_registers,byte_count=byte_count,payload1=payload1,payload2=payload2)
 
-write_modbus(slave_address=0x1,function_code=0x10,starting_address=0x1073,quantity_of_registers=0x2,byte_count=0x4,payload1=0x0000,payload2=0x0000)
+write_modbus(slave_address=0x1,function_code=0x10,starting_address=0x1073,quantity_of_registers=0x0002,byte_count=0x04,payload1=0x0000,payload2=0x0000)
 #write_modbus(slave_address=0x01,function_code=function_code,starting_address=0x020A,quantity_of_registers=0x01,byte_count=0x0002,payload=0x04)
 # Close the serial port
 

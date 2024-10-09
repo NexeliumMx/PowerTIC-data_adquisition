87aa50588c82c15f55fc1e7b51419cbf468fc0f3
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-10-09 15:59:35 -0600

    try for

--- Rapberry/resetregisters.py
+++ Rapberry/resetregisters.py
@@ -22,8 +22,9 @@ else:
     print("Failed to connect to Modbus client")
 
 # Define the register address and the value to write
-register_address = 0x1073  # The first register address (adjust for 0-based if necessary)
+#register_address = 0x1073  # The first register address (adjust for 0-based if necessary)
 value_to_write = 0x0000
+for register_address in range(4203,4217)
 value_high = (value_to_write >> 8) & 0xFF
 value_low = (value_to_write & 0xFF) 
 response = client.write_registers(register_address, [value_high,value_low], 1)

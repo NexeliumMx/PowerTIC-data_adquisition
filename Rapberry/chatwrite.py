a3f9b5b63392c6ad3b0a481d68e31cf946f52fc3
MemoDADR <memodealbadelriego@hotmail.com>
2024-10-26 18:09:44 -0600

    trys

--- Rapberry/chatwrite.py
+++ Rapberry/chatwrite.py
@@ -1,5 +1,5 @@
 import serial
-from chatread import modbus_read
+from modbus16bit import modbus_read,write_modbus
 import time
 # Serial port configuration (as before)
 ser = serial.Serial(
@@ -19,57 +19,6 @@ byte_count = 0x4
 payload1 = 0x0002
 payload2 = 0x0000
 
-def write_modbus(slave_address, function_code, starting_address, quantity_of_registers, byte_count, payload1, payload2):
-    def compute_crc(data):
-        crc = 0xFFFF
-        for pos in data:
-            crc ^= pos
-            for _ in range(8):
-                if crc & 0x0001:
-                    crc >>= 1
-                    crc ^= 0xA001
-                else:
-                    crc >>= 1
-        return crc
-
-
-
-
-    # Build the message (adjusted if necessary)
-    #format: Addr|Fun|Data start reg hi|Data start reg lo|Data # of regs hi|Data # of regs lo|Byte Count|Value Hi|Value Lo|CRC16 Hi|CRC16 Lo
-    message = bytearray()
-    message.append(slave_address)
-    message.append(function_code)
-    message.append((starting_address >> 8) & 0xFF)  # Starting address high byte
-    message.append(starting_address & 0xFF)          # Starting address low byte
-    message.append((quantity_of_registers >> 8) & 0xFF)  # Quantity high byte
-    message.append(quantity_of_registers & 0xFF)         # Quantity low byte
-    message.append(byte_count)                           # Byte Count parameter
-    message.append((payload1 >> 8) & 0xFF)                # Payload 1  high
-    message.append(payload1 & 0xFF)
-    message.append((payload2 >> 8) & 0xFF)                # Payload 2 high
-    message.append(payload2 & 0xFF)
-    # If your device requires additional fields, include them here
-    # For example, if a Byte Count is required:
-    # message.append(0x00)  # Byte Count (speculative)
-
-    # Compute CRC16 checksum
-    crc = compute_crc(message)
-    crc_low = crc & 0xFF
-    crc_high = (crc >> 8) & 0xFF
-
-    # Append CRC to the message
-    message.append(crc_low)
-    message.append(crc_high)
-
-    print("Sent: ", message)
-
-    # Send the message over serial port
-    ser.write(message)
-
-    # Read the response
-    response = ser.read(5 + (quantity_of_registers * 2) + 2)  # Adjust length as needed
-    print("Received:", response)
 
 def write_modbus_reset(slave_address, function_code, starting_address, quantity_of_registers, byte_count, payload1, payload2):
     def compute_crc(data):
---
+++ Rapberry/deploy copy.py
@@ -0,0 +1,21 @@
+from ComsTest import reading_meter
+import os
+from encript import encript
+def rundeploy(SN):
+    fil = open(r"vals/sn.txt", "r")
+    sn= fil.read()
+    if  os.path.exists(r'vals/sn.txt'):
+        f=open(r"vals/sn.txt","r")
+        sn=f.readline(1)
+        print('seviene')
+        print(sn)
+        reading_meter(SN)
+    else :
+        print('run imprint')
+    if os.path.isdir(r"vals/nfail"):
+        if not os.listdir(r"vals/nfail"):
+            print("Directory is empty")
+        else:    
+            print("Directory is not empty")
+    else:
+        print("Given directory doesn't exist")

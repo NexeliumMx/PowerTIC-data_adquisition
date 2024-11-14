4cba022e085e268137d78cd532602035a4cfaecf
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-11-14 11:22:51 -0600

    try

--- Rapberry/decode_modbus.py
+++ Rapberry/decode_modbus.py
@@ -37,9 +37,8 @@ def modbus_read(slave_address:int, function_code:int, starting_address:int, quan
     crc_high = (crc >> 8) & 0xFF
 
     # Append CRC to the message
-    message.append(crc_high)
     message.append(crc_low)
-    
+    message.append(crc_high)
 
     print("Sent: ", message)
 
@@ -57,4 +56,4 @@ def modbus_read(slave_address:int, function_code:int, starting_address:int, quan
 slave_address = 0x03
 function_code = 0x03
 
-modbus_read(slave_address=slave_address,function_code=function_code,starting_address=0x0002,quantity_of_registers=1)
+modbus_read(slave_address=slave_address,function_code=function_code,starting_address=0x0033,quantity_of_registers=1)

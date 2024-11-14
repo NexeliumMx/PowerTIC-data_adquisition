6165dd77229155af530b1a5aa01a46ff92a27522
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-11-14 15:00:21 -0600

    try

--- Rapberry/decode_modbus.py
+++ Rapberry/decode_modbus.py
@@ -74,4 +74,4 @@ def modbus_read(slave_address:int, function_code:int, starting_address:int, quan
 slave_address = 0x05
 function_code = 0x03
 
-modbus_read(slave_address=slave_address,function_code=function_code,starting_address=0x104C,quantity_of_registers=1)
+modbus_read(slave_address=slave_address,function_code=function_code,starting_address=0x106D,quantity_of_registers=2)

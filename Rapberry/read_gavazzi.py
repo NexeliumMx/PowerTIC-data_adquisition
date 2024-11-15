aa339707fa710b22ed192771f8e028aa9dd2fdf6
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-11-14 23:41:39 -0600

    try pymodbus

---
+++ Rapberry/read_gavazzi.py
@@ -0,0 +1,19 @@
+from pymodbus.client import ModbusSerialClient
+
+#Conexión con el medidor mediante modbus
+client = ModbusSerialClient(
+    port='/dev/ttyUSB0',
+    baudrate=19200,
+    parity='N',
+    stopbits=1,
+    bytesize=8,
+    timeout=5
+)
+
+
+result = client.read_holding_registers(0x0002,1,3)
+if not result.isError():
+    value = result.registers[0]
+    print("value: ", value)
+else: 
+    print("Communication error", result)

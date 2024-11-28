8f80f415c1783b83aa88d5b480ed0dfea409e289
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-11-28 16:29:56 -0600

    try

--- Rapberry/decode_modbus.py
+++ Rapberry/decode_modbus.py
@@ -101,7 +101,7 @@ def modbus_multiple_read(slave_address: int):
         bytesize=serial.EIGHTBITS,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
-        timeout=0.05
+        timeout=0.1
     ) as ser:
         for address in commands:
             logger.debug(f"Processing address: {address}")

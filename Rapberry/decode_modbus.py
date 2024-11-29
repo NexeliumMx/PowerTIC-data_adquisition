3d3249d9241d6f4568f738859c8251c878c29901
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-11-29 11:46:51 -0600

    try

--- Rapberry/decode_modbus.py
+++ Rapberry/decode_modbus.py
@@ -94,6 +94,10 @@ def decode_modbus_response(response, slave_address: int, datatype: str):
             data_value = struct.unpack('>I', bytes(data_bytes[:4]))[0]
         elif datatype.lower() == 'string':
             data_value = ''.join(chr(b) for b in data_bytes if b != 0)
+        elif datatype.lower() == 'acc32':
+            if len(data_bytes) != 4:
+                raise ValueError("ACC32 data length invalid")
+            data_value = struct.unpack('>I', bytes(data_bytes[:4]))[0]
         else:
             data_value = data_bytes  # Raw bytes
     except struct.error as e:

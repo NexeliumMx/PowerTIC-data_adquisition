c8a4e20cbcab495dd858c049dd130b614892284a
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-11-29 15:30:00 -0600

    try

--- Rapberry/modbus16bit.py
+++ Rapberry/modbus16bit.py
@@ -86,7 +86,7 @@ def write_modbus(slave_address:int, function_code:int, starting_address:int, qua
     response = ser.read(5 + (quantity_of_registers * 2) + 2)  # Adjust length as needed
     print("Received:", response)
 
-"""def write_modbus(slave_address:int, function_code:int, starting_address:int, quantity_of_registers:int, byte_count:int, payload1:int, payload2:int, payload3:int,payload4:int,payload5:int):
+def write_single_modbus(slave_address:int, function_code:int, starting_address:int, quantity_of_registers:int, payload:int):
     # Build the message (adjusted if necessary)
     #format: Addr|Fun|Data start reg hi|Data start reg lo|Data # of regs hi|Data # of regs lo|Byte Count|Value Hi|Value Lo|CRC16 Hi|CRC16 Lo
     message = bytearray()
@@ -94,20 +94,11 @@ def write_modbus(slave_address:int, function_code:int, starting_address:int, qua
     message.append(function_code)
     message.append((starting_address >> 8) & 0xFF)  # Starting address high byte
     message.append(starting_address & 0xFF)          # Starting address low byte
-    message.append((quantity_of_registers >> 8) & 0xFF)  # Quantity high byte
-    message.append(quantity_of_registers & 0xFF)         # Quantity low byte
-    message.append(byte_count)  
-    message.append((payload1>> 8) & 0xFF)                # Payload 1  high
-    message.append(payload1& 0xFF)
-    message.append((payload2>> 8) & 0xFF)                # Payload 2  high
-    message.append(payload2& 0xFF)
-    message.append((payload3>> 8) & 0xFF)                # Payload 3  high
-    message.append(payload3& 0xFF)
-    message.append((payload4>> 8) & 0xFF)                # Payload 4  high
-    message.append(payload4& 0xFF)
-    message.append((payload5>> 8) & 0xFF)                # Payload 5  high
-    message.append(payload5& 0xFF)
-    
+    #message.append((quantity_of_registers >> 8) & 0xFF)  # Quantity high byte
+    #message.append(quantity_of_registers & 0xFF)         # Quantity low byte
+    message.append((payload>> 8) & 0xFF)                # Payload 1  high
+    message.append(payload& 0xFF)
+
     # Compute CRC16 checksum
     crc = compute_crc(message)
     crc_low = crc & 0xFF
@@ -124,13 +115,14 @@ def write_modbus(slave_address:int, function_code:int, starting_address:int, qua
 
     # Read the response
     response = ser.read(5 + (quantity_of_registers * 2) + 2)  # Adjust length as needed
-    print("Received:", response)"""
+    print("Received:", response)
 #Reset meter attempt
 #write_modbus(slave_address=0x05,function_code=0x10,starting_address=0x209,quantity_of_registers=0x05,byte_count=0xA,payload1=0x0000,payload2=0x0000,payload3=0x0000,payload4=0x0000,payload5=0x0000)
 
 #current transform set
 #read
 modbus_read(slave_address=0x03,function_code=0x03,starting_address=0x1003,quantity_of_registers=0x0001)
-write_modbus(slave_address=0x03,function_code=0x10,starting_address=0x1003,quantity_of_registers=0x0001,byte_count=0x0002,payload=[0x00C8])
+#write_modbus(slave_address=0x03,function_code=0x10,starting_address=0x1003,quantity_of_registers=0x0001,byte_count=0x0002,payload=[0x00C8])
+write_single_modbus(slave_address=0x03,function_code=0x06,starting_address=0x1003,quantity_of_registers=0x0001,payload=[0x00C8])
 # Close the serial port
 ser.close()

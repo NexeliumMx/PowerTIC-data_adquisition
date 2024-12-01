a3db783b825ced46c5113211d85566cc78d4db33
MemoDADR <memodealbadelriego@hotmail.com>
2024-12-01 17:05:53 -0600

    t

--- Rapberry/ComsTest.py
+++ Rapberry/ComsTest.py
@@ -81,6 +81,7 @@ def meter_param(model,mbdadd):
                             if int(row["register_length"])>1:
                                 for i in range(0,int(row["register_length"])):
                                     #print("Modbus Address: ", modbus_address)
+                                    print('aqui')
                                     try:
                                         result = client.read_holding_registers(modbus_address+i, mbdadd)
                                         if not result.isError():

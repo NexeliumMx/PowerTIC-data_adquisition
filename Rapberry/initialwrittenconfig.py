896e62b600bd298c2b5030f7037f5c6880726903
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-12-02 13:52:43 -0600

    try

--- Rapberry/initialwrittenconfig.py
+++ Rapberry/initialwrittenconfig.py
@@ -1,7 +1,7 @@
 from modbus16bit import write_modbus_multiple 
 import json
 import csv
-from decode_modbus import modbus_multiple_read
+from decode_modbus import decode_modbus_response
 
 
 
@@ -38,7 +38,7 @@ def initialaddres(model,addrs):
                                                     
                         try:
                             result = write_modbus_multiple(1,row["write_command"],modbus_address,1,2,addrs)
-                            modbus_multiple_read(slave_address=slave_address)
+                            decode_modbus_response(response=result,slave_address=addrs,datatype='')
                             print(result)
                         except ValueError:
                             print(f"Invalid address for {parameter}: {modbus_address}")

a8312a7e8ade21c44236fdf93a329b86cce99dee
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-12-02 18:21:21 -0600

    a

--- Rapberry/decode_modbus.py
+++ Rapberry/decode_modbus.py
@@ -117,8 +117,8 @@ def decode_modbus_response(response, slave_address: int, datatype: str):
                 #logger.error(f"Invalid data length for int32 {len(data_bytes)} ------------------------------------")
                 #return
                 data_value = struct.unpack('>i', data_bytes[:4])[0]
-            else:
-                data_value = struct.unpack('>i', data_bytes[:4])[0]"""
+            else:"""
+            data_value = value#struct.unpack('>i', data_bytes[:4])[0]
         elif datatype.lower() in ['int16', 'sunssf']:
             if len(data_bytes) != 2:
                 raise ValueError("int16 requires exactly 2 bytes of data------------------------------------")

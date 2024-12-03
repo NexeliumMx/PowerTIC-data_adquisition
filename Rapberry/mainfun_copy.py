a0380fadaaee827bd440a960bd8bf9d8b39f05b3
MemoDADR <memodealbadelriego@hotmail.com>
2024-12-02 20:26:51 -0600

    test

--- Rapberry/mainfun_copy.py
+++ Rapberry/mainfun_copy.py
@@ -17,6 +17,9 @@ if not os.path.isdir(r'vals'):
                      capture_output=True))
     print(subprocess.run(["mkdir", "vals/success"], 
                      capture_output=True))
+a=requests.get('https://power-tick-api-js.nexelium.mx/api/testDBconnection')
+print(a.json())    
+
 subprocess.run(["curl","-O -J \"https://powertick-api-py.azurewebsites.net/api/downloadModbusRTUcsv\""],capture_output=True)
 r = requests.get('https://powertick-api-js.azurewebsites.net/api/supportedModels')
     

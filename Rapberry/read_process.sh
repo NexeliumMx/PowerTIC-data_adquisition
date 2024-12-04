83aea3e35a27b87985979f963ddc86d73350e51c
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-12-04 14:44:47 -0600

    a

--- Rapberry/read_process.sh
+++ Rapberry/read_process.sh
@@ -5,6 +5,7 @@ source /home/pi/MICO/.venv/bin/activate || { echo "Failed to activate virtual en
 cd /home/pi/MICO/PowerTIC-data_adquisition/Rapberry/
 # Run modbus16bit.py
 if ! python ./modbus16bit.py; then
+    free -h
     echo "modbus16bit.py failed"
     exit 1
 fi

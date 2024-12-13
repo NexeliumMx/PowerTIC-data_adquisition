2da464e30f0c9933a7042df6371c48c6f8ae50c8
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-12-13 12:44:35 -0600

    try

--- Rapberry/ComsTest.py
+++ Rapberry/ComsTest.py
@@ -109,7 +109,7 @@ def meter_param(model:str,mbadd:int):
                 if response:
                     #logger.debug(f"Received: {response}")
                     status = decode_modbus_response(response, mbadd, datatype, parameter)
-                    if status != "Incorrect CRC" and "":
+                    if status != "Incorrect CRC" and "Incomplete response received":
                         #logger.debug(f"Status: ", status)
                         settings[f"{parameter}"] = status
                         #print(settings)

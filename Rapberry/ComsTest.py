dec24122b26aa1be57cffce8a0ae24d5d25f3884
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2024-12-20 13:16:10 -0600

    try

--- Rapberry/ComsTest.py
+++ Rapberry/ComsTest.py
@@ -144,7 +144,7 @@ def meter_param(model:str,mbadd:int):
 
         json_data = settings
         print("JSON data: ", json_data)
-
+        print("reset", json_data['reset'])
         if json_data['reset']:
             logger.info(f'reset command found')
             del json_data['reset']

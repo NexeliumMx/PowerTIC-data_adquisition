6dd9bdbb247bc04c2b7e7bd270c219a885e0d625
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2025-01-16 15:02:33 -0600

    fix

--- Rapberry/version_update.sh
+++ Rapberry/version_update.sh
@@ -8,11 +8,11 @@ echo "Execution started at $(date)" >> $LOG_FILE
 
 # Log the stat command output
 echo "Checking initial file status..." >> $LOG_FILE
-stat ./mainfun_copy.dist/Powertick.bin >> $LOG_FILE 2>&1
+stat ./Powertick.dist/Powertick.bin >> $LOG_FILE 2>&1
 
 # Remove the file and log the result
 echo "Removing file..." >> $LOG_FILE
-rm ./mainfun_copy.dist/Powertick.bin >> $LOG_FILE 2>&1
+rm ./Powertick.dist/Powertick.bin >> $LOG_FILE 2>&1
 
 # Execute the Python script and log its output
 echo "Running image_download.py..." >> $LOG_FILE
@@ -23,8 +23,8 @@ echo "File Downloaded successfully" >> $LOG_FILE
 
 # Log the final stat command output
 echo "Checking final file status..." >> $LOG_FILE
-stat ./mainfun_copy.dist/Powertick.bin >> $LOG_FILE 2>&1
-chmod ./mainfun_copy.dist/Powertick.bin >> $LOG_FILE 
+stat ./Powertick.dist/Powertick.bin >> $LOG_FILE 2>&1
+chmod ./Powertick.dist/Powertick.bin >> $LOG_FILE 
 # Log the ending time
 echo "Execution ended at $(date)" >> $LOG_FILE
 

8e4136ef3467f9dd378c890d35fa02a1348d2e03
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2025-01-16 15:15:31 -0600

    gix

--- Rapberry/image_download.py
+++ Rapberry/image_download.py
@@ -18,7 +18,7 @@ def post_blob_request(file_name):
         bool: True if successful, False otherwise.
     """
     # Local file to save the downloaded blob content
-    DOWNLOAD_PATH = f"./{file_name}"
+    DOWNLOAD_PATH = f"./Powertick.dist/{file_name}"
     try:
         # JSON payload
         payload = {"file": file_name}

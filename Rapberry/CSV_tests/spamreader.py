40b42499fbc017d235766ec6cbc415973ad57807
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2025-03-11 20:31:50 -0600

    remove unused CSV and Python files; add python3-serial to apt-requirements

--- Rapberry/CSV_tests/spamreader.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open('/Users/luissanchez/MICO/PowerTIC/Rapberry/Modbusqueries.csv', newline='') as csvfile:
-    modbus_queries = csv.DictReader(csvfile)
-    for row in modbus_queries:
-        print(row['parameter_description'], row['modbus_address'])
